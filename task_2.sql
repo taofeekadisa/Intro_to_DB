8f5dba853a335f1e8f9f5432d0ccb66698846682
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-28 09:46:16 +0300

    update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -48,5 +48,16 @@ CREATE TABLE Orders (
   customer_id INT,
   order_date DATE,
   PRIMARY KEY (order_id),
-  FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+
+  orderdetailid INT NOT NULL,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  PRIMARY KEY (orderdetailid),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
