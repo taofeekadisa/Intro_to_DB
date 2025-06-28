0e18c55835bdf14914c3eb1d489b580a5d241349
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-28 10:11:47 +0300

    update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -16,11 +16,12 @@ SELECT
     COLUMN_NAME AS 'Column Name',
     COLUMN_TYPE AS 'Column Type',
     IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
     COLUMN_DEFAULT AS 'Default Value',
     EXTRA AS 'Extra Information'
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'Books';
 
