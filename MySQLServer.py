6918c30470937c8cb928e4bd9dadf05936ff9cb3
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-29 01:58:41 +0300

    update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -37,14 +37,13 @@ try:
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
+        cursor.close()
 
 except mysql.connector.Error as err:
     print("Failed creating database: {}".format(err))
 
 finally:
-    if 'cursor' in locals():
-        cursor.close()
-    if 'connection' in locals() and connection.is_connected():
+    if connection.is_connected():
         connection.close()
 
 
