d17154ae0cdfb55cd0d03cf21f373d609106504f
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-28 10:40:13 +0300

    update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -10,18 +10,19 @@ The name of the file should be task_4.sql
 All SQL keywords should be in uppercase
 
 """
-USE alx_book_store;
+
+USE your_database_name;
+
 
 SELECT 
     COLUMN_NAME AS 'Column Name',
     COLUMN_TYPE AS 'Column Type',
     IS_NULLABLE AS 'Nullable',
-    COLUMN_KEY AS 'Key',
     COLUMN_DEFAULT AS 'Default Value',
     EXTRA AS 'Extra Information'
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE() 
+    TABLE_SCHEMA = 'alx_book_store' 
     AND TABLE_NAME = 'Books';
 
