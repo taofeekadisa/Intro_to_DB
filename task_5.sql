6689f11670a41890c6cb6ae3ff4ff6463e36f9ed
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-29 09:16:56 +0300

    update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,24 +1,5 @@
-"""
-
-5. Populating Your Database with Your Very First Data
-mandatory
-Write a script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.
-
-Singel data insertion
-
-customer_id = 1
-customer_name = Cole Baidoo
-email = cbaidoo@sandtech.com
-address = 123 Happiness Ave.
-The database name will be passed as an argument of the mysql command
-
-Name of the file should be task_5.sql
-
-All SQL keywords should be in uppercase
-
-"""
 
 USE alx_book_store;
 
-INSERT INTO customers (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
