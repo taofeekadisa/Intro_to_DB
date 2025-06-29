ac0e0b5004b86a7ffdfb7c6de79b36c8bd9e3422
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-29 09:08:45 +0300

    update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,3 @@
-"""
-
-4. Full description
-mandatory
-Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
-
-The database name will be passed as an argument of the mysql command
-You are not allowed to use the DESCRIBE or EXPLAIN statements
-The name of the file should be task_4.sql
-All SQL keywords should be in uppercase
-
-"""
 
 SELECT 
     TABLE_NAME,
