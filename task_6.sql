54cff764c09b435075179a8cab916adea8864020
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-29 09:19:48 +0300

    update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,29 +1,3 @@
-"""
-
-6. Populating Your Database with Your More Data
-mandatory
-Write a script that inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.
-
-Data insertion
-
-    - `customer_id` = `2`
-    - `customer_name` = `Blessing Malik`
-    - `email` = `bmalik@sandtech.com`
-    - `address` = `124 Happiness  Ave.`
-    - `customer_id` = `3`
-    - `customer_name` = `Obed Ehoneah`
-    - `email` = `eobed@sandtech.com`
-    - `address` = `125 Happiness  Ave.`
-    - `customer_id` = `4`
-    - `customer_name` = `Nehemial Kamolu`
-    - `email` = `nkamolu@sandtech.com`
-    - `address` = `126 Happiness  Ave.`
-The database name will be passed as an argument of the mysql command
-
-The name of the file should be task_6.sql
-
-
-"""
 
 USE alx_book_store;
 
