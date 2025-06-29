df26408e98e70ee1b0913d2cd6ef0d8326c8ce35
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-29 09:10:31 +0300

    update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,22 +1,3 @@
-"""
-
-1. Let's Build Your Database: Your Gateway to Data Adventure!
-mandatory
-Write a simple python script that creates the database alx_book_store in your MySQL server.
-
-Name of python script should be MySQLServer.py
-If the database alx_book_store already exists, your script should not fail
-You are not allowed to use the SELECT or SHOW statements
-NOTE :
-
-Required to print message such as Database 'alx_book_store' created successfully! when database is successfully created.
-
-Print error message to handle errors when failing to connect to the DB.
-
-handle open and close of the DB in your script.
-
-"""
-
 
 import mysql.connector
 from mysql.connector import Error
